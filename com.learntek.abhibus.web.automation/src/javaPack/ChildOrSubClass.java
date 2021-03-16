8a9fddcb95e456da2f04cb3d406557c8877f0087
Manikandan Periyasamy <mani.alagarece@gmail.com>
2021-03-16 07:33:40 +0530

    Inheritance with a sum method

---
+++ com.learntek.abhibus.web.automation/src/javaPack/ChildOrSubClass.java
@@ -0,0 +1,12 @@
+package javaPack;
+
+public class ChildOrSubClass extends PrentOrSuperClass {
+
+	public static void main(String[] args) {
+
+		ChildOrSubClass childObj = new ChildOrSubClass();
+		childObj.sum();
+
+	}
+
+}
---
+++ com.learntek.abhibus.web.automation/src/javaPack/PrentOrSuperClass.java
@@ -0,0 +1,9 @@
+package javaPack;
+
+public class PrentOrSuperClass {
+
+	public void sum() {
+		System.out.println("This is sum method");
+	}
+
+}
