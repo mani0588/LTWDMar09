b1ac5a26efb71712abc16f757579ddcf4423f56a
Manikandan Periyasamy <mani.alagarece@gmail.com>
2021-03-18 06:44:51 +0530

    Selenium basic methods

--- web.automation/src/test/java/testPack/TestClass.java
+++ web.automation/src/test/java/testPack/TestClass.java
@@ -1,5 +1,6 @@
 package testPack;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,11 +8,31 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java");
+		WebDriver driver = new ChromeDriver(); // chrome browser opens
+		driver.get("https://www.mycontactform.com/samples.php");
+
+		driver.manage().window().maximize();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getPageSource());
+
+		// Navigation methods
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		Thread.sleep(3000);
+
+		// Boolean methods
+		System.out.println(driver.findElement(By.name("email_to[]")).isDisplayed()); // true
+		System.out.println(driver.findElement(By.name("email_to[]")).isEnabled()); // true
+		System.out.println(driver.findElement(By.name("email_to[]")).isSelected()); // false
+
+		driver.close();
 
 	}
 
