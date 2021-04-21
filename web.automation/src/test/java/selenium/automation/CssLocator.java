90acafc4c6732bc5d9b20c16cc4d7fc3308c50ab
Manikandan Periyasamy <mani.alagarece@gmail.com>
2021-04-21 07:28:51 +0530

    Css selector examples

--- web.automation/src/test/java/selenium/automation/CssLocator.java
+++ web.automation/src/test/java/selenium/automation/CssLocator.java
@@ -1,13 +1,33 @@
 package selenium.automation;
 
-public class CssLocator {
-	
-	
+import org.openqa.selenium.By;
+
+public class CssLocator extends DriverUtil {
+
 	public static void main(String[] args) {
-		
-		
-		
-		
+
+		getDriver();
+		driver.findElement(By.cssSelector("a")).sendKeys("Hello");
+
+		// # - by id
+		// . - by class name
+
+		// > - child only
+		// white-space - child and g-child
+
+		// parent and ancestor - not applicable in css
+
+		// preceding-sibling - not applicable in css
+		// label[for='subject'] + input ==> following-sibling
+
+		// selecting element by visible text not applicable in css
+
+		// a:nth-child(1) - based on position
+
+		// label[for^='sub'] - starts with
+		// label[for$='ect'] - ends with
+		// label[for*='ubj'] - contains
+
 	}
 
 }
